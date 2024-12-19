7870328ac79ccfc2d7c1a2abde248dce79557d2f
Geoff Maddock <geoff.maddock@gmail.com>
2024-12-19 14:33:47 -0500

    Add print queue script to check order of updates
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/geoff-maddock/advent-2024?shareId=XXXX-XXXX-XXXX-XXXX).

---
+++ day-5/print_queue.php
@@ -0,0 +1,49 @@
+<?php
+
+function parseInput($filename) {
+    $content = file_get_contents($filename);
+    list($rulesSection, $updatesSection) = explode("\n\n", $content);
+
+    $rules = array_map(function($line) {
+        return explode('|', trim($line));
+    }, explode("\n", $rulesSection));
+
+    $updates = array_map(function($line) {
+        return explode(',', trim($line));
+    }, explode("\n", $updatesSection));
+
+    return [$rules, $updates];
+}
+
+function isUpdateInOrder($update, $rules) {
+    $positions = array_flip($update);
+
+    foreach ($rules as $rule) {
+        list($x, $y) = $rule;
+        if (isset($positions[$x]) && isset($positions[$y]) && $positions[$x] > $positions[$y]) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function findMiddlePageNumber($update) {
+    $middleIndex = floor(count($update) / 2);
+    return $update[$middleIndex];
+}
+
+function main() {
+    list($rules, $updates) = parseInput('day-5/input.txt');
+    $sum = 0;
+
+    foreach ($updates as $update) {
+        if (isUpdateInOrder($update, $rules)) {
+            $sum += findMiddlePageNumber($update);
+        }
+    }
+
+    echo "The sum of the middle page numbers of the correctly-ordered updates is: $sum\n";
+}
+
+main();
